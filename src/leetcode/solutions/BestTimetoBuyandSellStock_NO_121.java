c0e51b022420644fbaca63067c7e38cbff4da29e
Igors Lohins <ilohins@users.noreply.github.com>
2024-04-19 23:23:07 -0400

    Added solution for 122

--- src/leetcode/solutions/BestTimetoBuyandSellStock_NO_121.java
+++ src/leetcode/solutions/BestTimetoBuyandSellStock_NO_121.java
@@ -2,7 +2,6 @@ package leetcode.solutions;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.List;
 
 public class BestTimetoBuyandSellStock_NO_121 {
