b508a1cc8a2ce0811b1c6aa52b43f2f46760f275
Igors Lohins <ilohins@users.noreply.github.com>
2024-08-19 23:10:01 -0400

    Solution for problem 125. Perf OK due to regexp

---
+++ src/main/java/leetcode/solutions/ValidPalindrome_NO_125.java
@@ -0,0 +1,20 @@
+package leetcode.solutions;
+
+public class ValidPalindrome_NO_125 {
+    public boolean isPalindrome(String s) {
+        String reduced = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
+        
+        for(int i = 0, j = reduced.length()-1; i < j; i++, j--) {
+        	if(reduced.charAt(i) != reduced.charAt(j)) {
+        		return false;
+        	}
+        }
+        
+        return true;
+    }
+    
+    public static void main(String[] args) {
+		ValidPalindrome_NO_125 s = new ValidPalindrome_NO_125();
+		System.out.println(s.isPalindrome(""));
+	}
+}
