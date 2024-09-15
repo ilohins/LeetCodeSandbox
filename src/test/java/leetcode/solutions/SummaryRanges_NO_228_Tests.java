71d24b533a288b6bb449dd758e4e6e27ac1cb0c8
Igors Lohins <ilohins@users.noreply.github.com>
2024-09-15 12:44:39 -0400

    Solution for 228

---
+++ src/test/java/leetcode/solutions/SummaryRanges_NO_228_Tests.java
@@ -0,0 +1,129 @@
+package leetcode.solutions;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Template class for testing, could be added as "Live Templates" to IDE
+ * 
+ * Includes performance testing in nano seconds
+ */
+public class SummaryRanges_NO_228_Tests {
+	private long timer = 0l;
+
+	public SummaryRanges_NO_228 solution = new SummaryRanges_NO_228();
+
+	@BeforeEach
+	public void init() {
+		System.out.println("JUNIT for " + this.getClass().getSimpleName() + " started");
+		timer = System.nanoTime();
+	}
+
+	@AfterEach
+	public void finalize() {
+		System.out.println(
+				"JUNIT for " + this.getClass().getSimpleName() + " completed in " + (System.nanoTime() - timer) + "ns");
+	}
+
+	/**
+	 * Tests provided by LeetCode
+	 */
+	@Test
+	public void testCanCompleteCircuit_given() {
+		{
+			int[] input = new int[] { 0, 1, 2, 4, 5, 7 };
+			List<String> result = solution.summaryRanges(input);
+			List<String> expected = Arrays.asList("0->2", "4->5", "7");
+			
+			String message = "Numbers: " + Arrays.toString(input) + "; result: " + Arrays.toString(result.toArray()) + "; expected: "
+					+ Arrays.toString(expected.toArray());
+
+			assertTrue(message, expected.containsAll(result) && result.containsAll(expected));
+		}
+
+		{
+			int[] input = new int[] { 0, 2, 3, 4, 6, 8, 9 };
+			List<String> result = solution.summaryRanges(input);
+			List<String> expected = Arrays.asList("0", "2->4", "6", "8->9");
+
+			String message = "Numbers: " + Arrays.toString(input) + "; result: " + Arrays.toString(result.toArray()) + "; expected: "
+					+ Arrays.toString(expected.toArray());
+
+			assertTrue(message, expected.containsAll(result) && result.containsAll(expected));
+		}
+	}
+
+	/**
+	 * Tests added by the developer
+	 */
+	@Test
+	public void testCanCompleteCircuit_added() {
+		{
+			int[] input = new int[] { 0, 1, 2, 3, 4};
+			List<String> result = solution.summaryRanges(input);
+			List<String> expected = Arrays.asList("0->4");
+
+			String message = "Numbers: " + Arrays.toString(input) + "; result: " + Arrays.toString(result.toArray()) + "; expected: "
+					+ Arrays.toString(expected.toArray());
+
+			assertTrue(message, expected.containsAll(result) && result.containsAll(expected));
+		}
+
+
+		{
+			int[] input = new int[] { 0, 2, 4, 6, 8, 10 };
+			List<String> result = solution.summaryRanges(input);
+			List<String> expected = Arrays.asList("0", "2", "4", "6", "8", "10");
+
+			String message = "Numbers: " + Arrays.toString(input) + "; result: " + Arrays.toString(result.toArray()) + "; expected: "
+					+ Arrays.toString(expected.toArray());
+
+			assertTrue(message, expected.containsAll(result) && result.containsAll(expected));
+		}
+		
+		{
+			int[] input = new int[] { 1, 2, 3, 4, 6, 7, 8, 9 };
+			List<String> result = solution.summaryRanges(input);
+			List<String> expected = Arrays.asList("1->4", "6->9");
+
+			String message = "Numbers: " + Arrays.toString(input) + "; result: " + Arrays.toString(result.toArray()) + "; expected: "
+					+ Arrays.toString(expected.toArray());
+
+			assertTrue(message, expected.containsAll(result) && result.containsAll(expected));
+		}
+	}
+
+	/**
+	 * Tests added by the developer to test unlikely scenarios
+	 */
+	@Test
+	public void testCanCompleteCircuit_negative() {
+		{
+			int[] input = new int[] { };
+			List<String> result = solution.summaryRanges(input);
+
+			assertTrue("Result not empty", result.size() == 0);
+		}
+		
+		{
+			int[] input = new int[] {1};
+			List<String> result = solution.summaryRanges(input);
+
+			assertTrue("Single element test", result.size() == 1 && "1".equals(result.get(0)));
+		}
+	}
+
+	/**
+	 * Tests from failed LeetCode run debugs (except performance)
+	 */
+	@Test
+	public void testCanCompleteCircuit_reruns() {
+
+	}
+}
