49721948d660bf6c360b79f6b580e69ba25b1dd8
Igors Lohins <igorslohinsswe@gmail.com>
2025-02-26 20:00:27 -0500

    Problem 873 LengthofLongestFibonacciSubsequence initial

---
+++ src/main/java/dev/ilswe/leetcode/solutions/mmxxv/LengthofLongestFibonacciSubsequence_NO_873.java
@@ -0,0 +1,7 @@
+package dev.ilswe.leetcode.solutions.mmxxv;
+
+public class LengthofLongestFibonacciSubsequence_NO_873 {
+    public int lenLongestFibSubseq(int[] arr) {
+        return 5;
+    }
+}
